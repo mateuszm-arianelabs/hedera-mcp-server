a761144b9bfd2335bdd02eb9725b0a5599395194
matevszm <mateusz.marcinkowski@blockydevs.com>
2025-04-11 09:22:25 +0200

    feat: refactor to SSE communication

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 import "dotenv/config"
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import express from "express"
 
 // Create an MCP server
 const server = new McpServer({
-  name: "Demo",
+  name: "Hedera MCP Server",
   version: "1.0.0"
 });
+
+const app = express();
 
 server.tool("interact-with-hedera", {
   fullPrompt: z.string()
 }, async ({ fullPrompt }) => {
   
   try {
-    const response = await fetch("http://localhost:3000/interact-with-hedera", {
+    const response = await fetch(`${process.env.API_URL}/interact-with-hedera`, {
       method: "POST",
       body: JSON.stringify({
         fullPrompt
@@ -42,5 +45,32 @@ server.tool("interact-with-hedera", {
   }
 })
 
-const transport = new StdioServerTransport();
-void server.connect(transport);
+// Communication layer
+
+type SessionId = string;
+// Handle multiple parallel sessions
+const transports: Map<SessionId, SSEServerTransport> = new Map() 
+
+app.get("/sse", async (req, res) => {
+    const transport = new SSEServerTransport('/messages', res);
+    transports.set(transport.sessionId, transport);
+    res.on("close", () => {
+        transports.delete(transport.sessionId);
+    });
+    await server.connect(transport);
+});
+
+// Main message handler
+app.post("/messages", async (req, res) => {
+    const sessionId = req.query.sessionId as SessionId;
+    const transport = transports.get(sessionId);
+    if (!transport) {
+        res.status(404).send("Transport not found");
+        return;
+    }
+
+    await transport.handlePostMessage(req, res);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
